7209f7cd2bc7f40e749fa2a195a8d7064657b193
Adrian <adrijaulig@gmail.com>
2023-01-27 10:50:13 +0100

    Principal_Commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [cards, setCards] = useState([]);
+const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    fetch("https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards")
-      .then((response) => {
-        return response.json();
-      })
-      .then((recurso) => {
-        setCards(recurso.cardsReverse);
-      });
-  }, []);
+useEffect(() => {
+  fetch("https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/")
+    .then(response => response.json())
+    .then(cards => setCards(cards));
+   // console.info(response.json())
+}, []);
+
+/* fetch("https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/10")
+  .then((response) => {
+    return response.json();
+  })
+  .then((recurso) => {
+    console.log(recurso.cardsReverse);
+  }); */
+
 
   return (
     <div className="App">
-      {cards.map((card) => (
-        <div key={card.id}>
-          <img src={card.image} alt={card.name} />
-          <p>{card.name}</p>
-        </div>
-      ))}
-    </div>
+    <p>{`Numero de cartas: ${cards.reduce((acc) => acc + 1, 0)}`}</p>   
+    {cards.map((card) => (
+      <div key={card.id}>
+       {<img src={card.cardsReverse.sakuraReverse} alt={card.name} />}
+       {/* {<img src={card.clowCard} alt={card.name} />} */}
+  
+        <p>{card.spanishName}</p>
+      </div>
+    ))}
+  </div>
   );
-}
+  }
+
 
 export default App;
 
